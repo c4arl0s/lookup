fbb90e14b7d11b0bcec23770c9cbf6aa5134bdfd
c4arl0s <c.santiago.cruz@gmail.com>
2026-08-05 18:03:07 -0600

    feat(menu): add interactive edit option for new words
    - Added an 'edit' option to the 'add new word' prompt.
    - Allowed users to interactively correct the English and Spanish fields in the terminal before finalizing the new word entry.

--- trad-functions/display_option_to_add_new_word.sh
+++ trad-functions/display_option_to_add_new_word.sh
@@ -4,6 +4,6 @@ display_option_to_add_new_word()
 {
   printf "\n${WHITE}${espanol} : ${ingles}"
   printf "\n${CYAN}Do you want to add this word?:${PINK} ${WORD} ?"
-  printf "%s" "Type your choice [yes|add3|↩(ignore)]: "; read decision
+  printf "%s" "Type your choice [yes|edit|add3|↩(ignore)]: "; read decision
   echo -e "${GREEN}"
 }
