fc9ac4c8e45c5e1b160757a7ee33812204e8595f
c4arl0s <c.santiago.cruz@gmail.com>
2026-08-05 18:15:23 -0600

    fix(audio): reproduce correct audio after adding a new word
    - Fixed an issue where the script reproduced the audio of the previously found word instead of the newly added one.
    - Corrected the variable case for 'last_word_found' which was preventing it from updating properly when adding unknown words.
    - Ensured the audio is only reproduced if the user confirms and successfully adds the word.

--- trad-functions/display_menu.sh
+++ trad-functions/display_menu.sh
@@ -4,8 +4,10 @@ display_menu()
 {
   case ${decision} in 
   "yes")  echo -e "${GREEN}It will add word: ${ingles}"
-          add_word "${espanol}" "${ingles}"
-          LAST_WORD_FOUND=${ingles} ;;
+          if add_word "${espanol}" "${ingles}"; then
+            last_word_found="${ingles}"
+            reproduce_english_audio_file_if_available "${ingles}"
+          fi ;;
   "edit") echo -e "${GREEN}Editing fields before adding..."
           printf "%s" "Type new English word [Current: ${ingles}]: "; read new_ingles
           [[ -n "$new_ingles" ]] && ingles="$new_ingles"
@@ -13,8 +15,10 @@ display_menu()
           printf "%s" "Type new Spanish translation [Current: ${espanol}]: "; read new_espanol
           [[ -n "$new_espanol" ]] && espanol="$new_espanol"
           
-          add_word "${espanol}" "${ingles}"
-          LAST_WORD_FOUND=${ingles} ;;
+          if add_word "${espanol}" "${ingles}"; then
+            last_word_found="${ingles}"
+            reproduce_english_audio_file_if_available "${ingles}"
+          fi ;;
   "no")   echo -e "${RED}You cancelled" ;;
   "add3") add3 ;; 
   *) 	    echo -e "${CYAN}You did type enter";;
